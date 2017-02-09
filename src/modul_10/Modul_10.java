251e6491f2756ab4d314c299e76e62c47607ad38
Samudra~ <Samudra~@192.168.21.1>
2017-02-09 08:39:08 +0700

    Final

---
+++ src/modul_10/Modul_10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul_10;
+
+/**
+ *
+ * @author Samudra~
+ */
+public class Modul_10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
